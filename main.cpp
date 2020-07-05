d13c26938b59b63759bdd7b700669ea74974e619
Orso Eric <30684972+OrsoEric@users.noreply.github.com>
2020-07-05 08:41:54 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,21 +5,21 @@ void init()
 {
 	//Clock the GPIO banks
 	rcu_periph_clock_enable(RCU_GPIOA);
-    rcu_periph_clock_enable(RCU_GPIOC);
-    //Setup the R, G and B LEDs
-    gpio_init(GPIOA, GPIO_MODE_OUT_PP, GPIO_OSPEED_50MHZ,GPIO_PIN_1);
-    gpio_init(GPIOA, GPIO_MODE_OUT_PP, GPIO_OSPEED_50MHZ,GPIO_PIN_2);
+	rcu_periph_clock_enable(RCU_GPIOC);
+	//Setup the R, G and B LEDs
+	gpio_init(GPIOA, GPIO_MODE_OUT_PP, GPIO_OSPEED_50MHZ,GPIO_PIN_1);
+	gpio_init(GPIOA, GPIO_MODE_OUT_PP, GPIO_OSPEED_50MHZ,GPIO_PIN_2);
 	gpio_init(GPIOA, GPIO_MODE_IN_FLOATING, GPIO_OSPEED_50MHZ,GPIO_PIN_8);
 	//Setup the boot button
 	gpio_init(GPIOC, GPIO_MODE_OUT_PP, GPIO_OSPEED_50MHZ,GPIO_PIN_13);
-    //Initialize the LEDs to: OFF
-    gpio_bit_set(GPIOC,GPIO_PIN_13);
-    gpio_bit_set(GPIOA,GPIO_PIN_1);
-    gpio_bit_set(GPIOA,GPIO_PIN_2);
-	
+	//Initialize the LEDs to: OFF
+	gpio_bit_set(GPIOC,GPIO_PIN_13);
+	gpio_bit_set(GPIOA,GPIO_PIN_1);
+	gpio_bit_set(GPIOA,GPIO_PIN_2);
+
 	//Clock the alternate functions
 	rcu_periph_clock_enable(RCU_AF);
-	
+
 	//Initialize the ECLIC IRQ lines
 	eclic_priority_group_set(ECLIC_PRIGROUP_LEVEL3_PRIO1);
 	eclic_irq_enable(EXTI5_9_IRQn, 1, 1);
@@ -27,10 +27,10 @@ void init()
 	//Initialize the EXTI. IRQ can be generated from GPIO edge detectors
 	gpio_exti_source_select(GPIO_PORT_SOURCE_GPIOA, GPIO_PIN_SOURCE_8);
 	exti_init(EXTI_8, EXTI_INTERRUPT, EXTI_TRIG_BOTH);
-	
+
 	//Clear interrupt flag. Ensure no spurious execution at start
 	exti_interrupt_flag_clear(EXTI_8);
-	
+
 	//Enable the interrupts. From now on interrupt handlers can be executed
 	eclic_global_interrupt_enable();	
 
@@ -57,23 +57,23 @@ void EXTI5_9_IRQHandler()
 
 void delay_us(unsigned int us)
 {
-    uint64_t start_mtime, delta_mtime;
-    
+	uint64_t start_mtime, delta_mtime;
+
 	uint64_t tmp = get_timer_value();
 
-    do
-    {
-        start_mtime = get_timer_value();
-    }
-    while (start_mtime == tmp);
+	do
+	{
+		start_mtime = get_timer_value();
+	}
+	while (start_mtime == tmp);
 
-    do
-    {
-        delta_mtime = get_timer_value() - start_mtime;
-    }
-    while(delta_mtime <(SystemCoreClock/4000000.0 *us ));
+	do
+	{
+		delta_mtime = get_timer_value() - start_mtime;
+	}
+	while(delta_mtime <(SystemCoreClock/4000000.0 *us ));
 
-    return;
+	return;
 }
 
 int main()
